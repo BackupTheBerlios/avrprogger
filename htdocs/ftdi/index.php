ec0338da6b566acbdc037865dce4d17d17d8cae7
hdietze <hdietze>
2003-07-03 19:47:37 +0000

    added subproject "FTDI"

--- htdocs/ftdi/index.php
+++ htdocs/ftdi/index.php
@@ -5,9 +5,7 @@
 <body>
 <?include 'menu.inc'?>
 
-<h1>AVR Progger</h1>
+<h1>USB-Serial converter</h1>
 
-<h2>Subprojects</h2>
-<li> <a href="ftdi/index.php">USB-Serial converter</a> </li>
 
 <?include 'footer.inc'?>
