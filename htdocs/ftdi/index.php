6795dc62d43df7f0ad6070ac38813df96cd9db36
hdietze <hdietze>
2004-05-04 20:31:05 +0000

    added link to FTDI

--- htdocs/ftdi/index.php
+++ htdocs/ftdi/index.php
@@ -3,9 +3,10 @@
 <title>AVR Progger</title>
 </head>
 <body>
-<?include 'menu.inc'?>
+<?include '../menu.inc'?>
 
 <h1>USB-Serial converter</h1>
 
+<a href="http://www.ftdichip.com/">FTDI</a>
 
-<?include 'footer.inc'?>
+<?include '../footer.inc'?>
